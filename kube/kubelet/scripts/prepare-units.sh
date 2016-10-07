8f299d6619e23b6c7ccc3a436b2ffedc6a3e3d70
Cedric Hauber <cedric.hauber@wescale.fr>
2016-10-07 16:58:00 +0200

    Add private & public

--- kube/kubelet/scripts/prepare-units.sh
+++ kube/kubelet/scripts/prepare-units.sh
@@ -3,4 +3,6 @@ source /etc/pidalio.env
 for file in $(ls /opt/pidalio/kube/units/master/*.service)
 do
     sed -i s/\\\$token\\\$/${PIDALIO_TOKEN}/g ${file}
+    sed -i s/\\\$private_ipv4\\\$/${NODE_IP}/g ${file}
+    sed -i s/\\\$public_ipv4\\\$/${NODE_PUBLIC_IP}/g ${file}
 done
