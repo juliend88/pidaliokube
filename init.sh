14abae65ebeb36430d8d2433aafc2112368bd988
Cedric Hauber <cedric.hauber@wescale.fr>
2016-10-12 17:07:58 +0200

    Fix etcd

--- init.sh
+++ init.sh
@@ -17,19 +17,14 @@ EXISTING_IPS=$(/opt/bin/weave dns-lookup etcd | sort)
 EXISTING_IDS=""
 for ip in ${EXISTING_IPS}
 do
-    ETCD_UP=0
     for f in {1..10}; do
         sleep 1
         if curl -s -m 1 http://${ip}:2379/v2/stats/self
         then
-            ETCD_UP=1
-            break
+            echo "Etcd $ip did not come up...exiting"
+            exit 1
         fi
     done
-    if [ "$ETCD_UP" -eq 0 ]; then
-        echo "Etcd $ip did not come up...ignoring"
-        continue
-    fi
     IP_ID=$(curl -s -m 10 http://${ip}:2379/v2/stats/self | jq -r .name | cut -d'-' -f 2)
     EXISTING_IDS=${IP_ID},${EXISTING_IDS}
     echo "Etcd $ip already exist, ID: $IP_ID";
