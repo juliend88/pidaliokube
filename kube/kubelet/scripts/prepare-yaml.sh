b6e608ab1512b78e78180aefab9eda1d60a1f146
Cedric Hauber <cedric.hauber@wescale.fr>
2016-11-24 11:08:06 +0100

    Add stack-name env variable

--- kube/kubelet/scripts/prepare-yaml.sh
+++ kube/kubelet/scripts/prepare-yaml.sh
@@ -3,6 +3,7 @@ rm -Rf /etc/kubernetes/descriptors/*
 cp -Rf /opt/pidalio/kube/kubelet/descriptors/* /etc/kubernetes/descriptors
 for file in $(ls /etc/kubernetes/descriptors/dns/*.yaml /etc/kubernetes/descriptors/toolbox/*.yaml /etc/kubernetes/descriptors/ceph/*.yaml)
 do
+    sed -i s/\\\$stack_name\\\$/${STACK_NAME}/g ${file}
     sed -i s/\\\$token\\\$/${PIDALIO_TOKEN}/g ${file}
     sed -i s/\\\$domain\\\$/${DOMAIN}/g ${file}
     sed -i s/\\\$node_type\\\$/${NODE_TYPE}/g ${file}
