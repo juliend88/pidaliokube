af448190f51181d280857f2bd1015260d3cb522e
Cedric Hauber <cedric.hauber@wescale.fr>
2016-11-28 15:57:54 +0100

    Fix dns

--- kube/kubelet/scripts/launch-kubelet.sh
+++ kube/kubelet/scripts/launch-kubelet.sh
@@ -33,6 +33,7 @@ echo "Setting DNS"
 WEAVE_DNS_ADDRESS=$(/opt/bin/weave report | jq -r .DNS.Address | cut -d ':' -f 1)
 cat <<EOF > /etc/resolv.conf
 nameserver ${WEAVE_DNS_ADDRESS}
+nameserver 185.23.94.244
 EOF
 /opt/bin/kubelet \
     --network-plugin=cni \
--- kube/kubelet/scripts/launch-proxy.sh
+++ kube/kubelet/scripts/launch-proxy.sh
@@ -14,6 +14,7 @@ echo "Setting DNS"
 WEAVE_DNS_ADDRESS=$(/opt/bin/weave report | jq -r .DNS.Address | cut -d ':' -f 1)
 cat <<EOF > /etc/resolv.conf
 nameserver ${WEAVE_DNS_ADDRESS}
+nameserver 185.23.94.244
 EOF
 /opt/bin/kube-proxy \
   --master=https://pidalio-apiserver \
