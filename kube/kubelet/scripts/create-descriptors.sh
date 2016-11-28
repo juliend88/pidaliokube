d76f45bf836a32ebd7364cee345e11a11b131ac9
Cedric Hauber <cedric.hauber@wescale.fr>
2016-11-28 17:13:02 +0100

    Fix kubeconfig path

--- kube/kubelet/scripts/create-descriptors.sh
+++ kube/kubelet/scripts/create-descriptors.sh
@@ -8,14 +8,14 @@ do
 done
 if [[ $i == 5 ]]; then exit 1; fi
 # Initialize Kubernetes Addons
-/opt/bin/kubectl create -f /etc/kubernetes/descriptors/dns
+/opt/bin/kubectl --kubeconfig=/home/core/.kube/config create -f /etc/kubernetes/descriptors/dns
 # Initialize Ceph
 if [[ "${CEPH}" == "True" ]]
 then
     /opt/pidalio/kube/kubelet/scripts/ceph/install-ceph.sh
     if [[ "${CEPH_DISK}" == "True" ]]
     then
-        /opt/bin/kubectl --namespace=ceph create \
+        /opt/bin/kubectl --kubeconfig=/home/core/.kube/config --namespace=ceph create \
         -f /etc/kubernetes/descriptors/ceph/ceph-mds-v1-dp.yaml \
         -f /etc/kubernetes/descriptors/ceph/ceph-mon-check-v1-dp.yaml \
         -f /etc/kubernetes/descriptors/ceph/ceph-mon-v1-dp.yaml \
@@ -24,7 +24,7 @@ then
         -f /etc/kubernetes/descriptors/ceph/ceph-stats-v1-dp.yaml \
         -f /etc/kubernetes/descriptors/ceph/ceph-stats-v1-svc.yaml
     else
-        /opt/bin/kubectl --namespace=ceph create \
+        /opt/bin/kubectl --kubeconfig=/home/core/.kube/config --namespace=ceph create \
         -f /etc/kubernetes/descriptors/ceph/ceph-mds-v1-dp.yaml \
         -f /etc/kubernetes/descriptors/ceph/ceph-mon-check-v1-dp.yaml \
         -f /etc/kubernetes/descriptors/ceph/ceph-mon-v1-dp.yaml \
@@ -33,7 +33,7 @@ then
         -f /etc/kubernetes/descriptors/ceph/ceph-stats-v1-dp.yaml \
         -f /etc/kubernetes/descriptors/ceph/ceph-stats-v1-svc.yaml
     fi
-    until [ "$(/opt/bin/kubectl get pods --namespace=ceph | tail -n +2 | egrep -v '(.*)1/1(.*)Running' | wc -l)" == "0" ]
+    until [ "$(/opt/bin/kubectl --kubeconfig=/home/core/.kube/config get pods --namespace=ceph | tail -n +2 | egrep -v '(.*)1/1(.*)Running' | wc -l)" == "0" ]
     do
       echo "Waiting for ceph to be ready"
       sleep 10
@@ -57,12 +57,12 @@ then
           /opt/bin/rbd -m ceph-mon.ceph create grafana --size=1G
           sleep 10
         done
-        /opt/bin/kubectl create -f /etc/kubernetes/descriptors/monitoring --namespace=monitoring
+        /opt/bin/kubectl --kubeconfig=/home/core/.kube/config create -f /etc/kubernetes/descriptors/monitoring --namespace=monitoring
     fi
 fi
 # Initialize Toolbox
 ssh-keygen -t rsa -f key
-/opt/bin/kubectl create secret generic toolbox --from-file=ssh-privatekey=key --from-file=ssh-publickey=key.pub
+/opt/bin/kubectl --kubeconfig=/home/core/.kube/config create secret generic toolbox --from-file=ssh-privatekey=key --from-file=ssh-publickey=key.pub
 rm -f key key.pub
 # Openstack secrets
 source /etc/openstack.env
@@ -70,7 +70,7 @@ OS_USERNAME=$(echo -n $OS_USERNAME | base64)
 OS_PASSWORD=$(echo -n $OS_PASSWORD | base64)
 OS_AUTH_URL=$(echo -n $OS_AUTH_URL | base64)
 OS_TENANT_NAME=$(echo -n $OS_TENANT_NAME | base64)
-cat <<EOF | kubectl create -f -
+cat <<EOF | kubectl --kubeconfig=/home/core/.kube/config create -f -
   apiVersion: v1
   kind: Secret
   metadata:
@@ -82,5 +82,5 @@ cat <<EOF | kubectl create -f -
     password: $OS_PASSWORD
     username: $OS_USERNAME
 EOF
-/opt/bin/kubectl create -f /etc/kubernetes/descriptors/toolbox/
+/opt/bin/kubectl --kubeconfig=/home/core/.kube/config create -f /etc/kubernetes/descriptors/toolbox/
 exit 0
